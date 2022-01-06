3a03b7ca66fca6ecfab1472cf807643635c8801d
unknown <saniy99909@gmail.com>
2022-01-07 00:41:21 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useState} from 'react'
+import axios from 'axios';
 
 function App() {
+  const [image, setImage] = useState('')
+
+  const handleChange = () => {
+    axios.get('https://randomuser.me/api/')
+    .then(res => {
+      const uri = res.data.results[0].picture.large
+      uri && setImage(uri)
+    }).catch(err => {
+      console.log(err.message);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>AI Photo Generator</h1>
+      {image && <img src={image} alt="AI Face"/>}
+      <button type='button' onClick={handleChange}>
+        New Image
+      </button>
     </div>
   );
 }
